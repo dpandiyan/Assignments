8c5d1f4925db44b5c439d0de59418f614d65bc50
training <training@PC380344.cts.com>
2019-05-24 12:22:34 +0530

    FirstCommint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+  todoList = new Array()
+  completedList = new Array()
+  todoItem = ""
+
+  addItemToTodoList() {
+    console.log("inside addItemToTodoList")
+    console.log(this.todoItem)
+    if (this.todoItem != "") {
+      this.todoList.push(this.todoItem)
+      this.todoItem = ""
+      console.log(this.todoList)
+    }
+  }
+  addToCompletedList(index) {
+    this.completedList.push(this.todoList[index])
+    this.todoList.splice(index, 1)
+  }
+  removeFromCompletedList(index) {
+    this.completedList.splice(index, 1)
+  }
 }
